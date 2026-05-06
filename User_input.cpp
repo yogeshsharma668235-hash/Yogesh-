f9f668b5e779566c0af09d3872e90a617bf9a6a1
yogeshsharma668235-hash <223951993+yogeshsharma668235-hash@users.noreply.github.com>
2026-05-06 11:14:30 +0530

    Implement C++ user input logic

---
+++ User_input.cpp
@@ -0,0 +1,19 @@
+/* 
+* Program Name :User Input practice
+*Description :A simple C++ program to demonstrate input and output
+operation.
+* Author:Yogesh kaushik
+*/
+#include<iostream>
+ using namespace std;
+ int main(){
+    //Declare an integer variable to store the age
+    int age;
+    //Prompt the user to enter their age .
+    cout<<"Enter the age :";
+    //Take input form the user 
+    cin>>age;
+    //Display the output to the user.
+    cout<<"you are age is :"<<age <<endl;
+    return 0;
+ }
