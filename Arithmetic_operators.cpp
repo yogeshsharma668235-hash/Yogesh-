57549716a76728b6611f28d5fe79016b0c4e0e16
yogeshsharma668235-hash <223951993+yogeshsharma668235-hash@users.noreply.github.com>
2026-05-07 17:00:33 +0530

    implement arithmetic operation with comments

---
+++ Arithmetic_operators.cpp
@@ -0,0 +1,27 @@
+/* Program :Basic arithmetic operators in c++
+    Description : This program demonstrates the 
+    performs addition, subtraction, multiplication,division and
+    modulus operations on two integers and displays the results.
+    Author : Yogesh kaushik
+    Date : 07/05/2026
+*/
+#include<iostream>
+using namespace std;
+    //Variable declaration and initialization
+int main(){
+    int a=40 ,b=20;
+    //performing arithmetic operations
+    int sum=a+b;
+    //Displaying the result of addition
+    cout<<sum<<endl; //addition
+    // Displaying the result of subtraction
+    cout<<a-b<<endl; //subtraction
+    //Displayig the result of multiplication
+    cout<<a*b<<endl;//multiplication
+    //Displaying the result of division
+    cout<<a/b<<endl;//division
+    //Displaying the result of modulus
+    cout<<a%b<<endl;//modulus
+    //End of the program
+    return 0; 
+}
